ab861e6dc35f62ce853c49549fb95cf218389e0d
Lindsay <lindsay.lbm@gmail.com>
2019-10-24 15:03:02 -0500

    added to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,8 +4,8 @@
 
 var friendsArray = [
     {
-        "name": "Meg",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Braden",
+        "photo": "https://assets.entrepreneur.com/content/3x2/2000/20190502194704-ent19-june-editorsnote.jpeg",
         "scores": [
             5,
             1,
@@ -17,6 +17,70 @@ var friendsArray = [
             5,
             4,
             1
+        ]
+    },
+    {
+        "name": "Sam",
+        "photo": "https://carrotquinn.files.wordpress.com/2017/02/nicole-hike.jpg",
+        "scores": [
+            1,
+            3,
+            5,
+            2,
+            1,
+            1,
+            1,
+            1,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Domino",
+        "photo": "https://pbs.twimg.com/media/Do_2fOwUcAE4zbd.jpg",
+        "scores": [
+            2,
+            2,
+            2,
+            3,
+            5,
+            3,
+            2,
+            1,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "Hermione",
+        "photo": "https://imgix.bustle.com/rehost/2016/9/13/33785d76-556f-4ea4-8cf3-10d190897860.png?w=970&h=546&fit=crop&crop=faces&auto=format&q=70",
+        "scores": [
+            2,
+            2,
+            2,
+            4,
+            5,
+            1,
+            1,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Ralph",
+        "photo": "https://akns-images.eonline.com/eol_images/Entire_Site/2018128/rs_600x600-180228063409-600.wreck-it-ralph.22818.jpg?fit=around|700:700&crop=700:700;center,top&output-quality=90",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1,
+            2,
+            1,
+            4
         ]
     }
 ];
